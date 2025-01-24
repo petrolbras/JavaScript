e821d081a62e93e35378301dcb50a476b7c2808e
Petrolbras <pietrogouvearepleop@gmail.com>
2025-01-24 19:46:05 -0300

    att e for

---
+++ Aulas/Aula14/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+
+for(var c; c<=5 ; c++) {
+    console.log(c)
+}
+
+/*
+
+Mesma coisa que o
+
+var c = 1
+while ( c<=5 ) {
+    console.log(c)
+    c++
+}
+
+ou que o
+
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+*/
