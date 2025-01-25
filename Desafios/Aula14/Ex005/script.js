c6a33728a664610c61b086062a9c7a97b5cf4c0d
Petrolbras <pietrogouvearepleop@gmail.com>
2025-01-24 21:55:16 -0300

    exercício terminado

--- Desafios/Aula14/Ex005/script.js
+++ Desafios/Aula14/Ex005/script.js
@@ -5,16 +5,28 @@ function contar() {
     let res = document.getElementById('res')
     
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = ('[ERRO] Não é possível realizar essa conta!')
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Não é possível realizar essa conta!')
     }  else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando passo = 1')
+            p = 1
+        }
+       if (i < f) {
+        // contagem crescente
         for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+    } else {
+        // contagem decrescente
+        for(let c= i; c >= f; c -=p) {
+            res.innerHTML += `${c} \u{1F449}`
         }
     }
-
+    res.innerHTML += `\u{1f3c1}`
+    }
 }
