a5f3a57ec6346d2c23bef256c07897fd91d54622
Petrolbras <pietrogouvearepleop@gmail.com>
2025-01-28 19:29:18 -0300

    att

--- Aulas/Aula15/ambiente.js
+++ Aulas/Aula15/ambiente.js
@@ -1,7 +1,7 @@
 let num = [5, 2, 4, 8, 9]
 
-num.push(10)
 num.sort()
+num.push(10)
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições!`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
