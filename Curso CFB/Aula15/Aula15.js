732b52627e0ee186169a8dd7d9af0473706bc12f
Petrolbras <pietrogouvearepleop@gmail.com>
2025-02-08 16:46:49 -0300

    att

---
+++ Curso CFB/Aula15/Aula15.js
@@ -0,0 +1,7 @@
+console.log("Início do programa")
+
+for(let i = 0; i <= 10; i++){
+    console.log("CFB cursos - Valor do i: " + i)
+} 
+console.log("Fim do programa")
+//Enquanto for verdadeiro, ele irá executar
---
+++ Curso CFB/Aula17/Aula17.1.js
@@ -0,0 +1,11 @@
+
+let n = 4
+let fat=1
+
+while(n >= 1){
+    fat*=n
+    //fat=fat*n
+    n--
+}
+
+console.log(fat)
