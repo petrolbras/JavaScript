6e0ab0cce932b96b827e3b1f576110b5433bb996
Petrolbras <pietrogouvearepleop@gmail.com>
2025-02-08 12:36:36 -0300

    att

---
+++ Curso CFB/Aula12/Aula12.js
@@ -0,0 +1,8 @@
+let n1 = [10,20,30]
+let n2 = [11,22,32,40]
+let n3 = [...n1,...n2]
+
+//console.log("n1: " + n1)
+//console.log("n2: " + n2)
+console.log("n3: " + n3)
+console.log("Tipo de n3: " + typeof(n3))
