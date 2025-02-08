03a5d54f81561095d5fa0fe06ac3c5785b49ecd7
Petrolbras <pietrogouvearepleop@gmail.com>
2025-02-08 17:42:45 -0300

    att final

--- Curso CFB/Aula21/Aula21.js
+++ Curso CFB/Aula21/Aula21.js
@@ -1,7 +1,14 @@
-let n1 = 1
-
-while (n1 < 10) {
-    console.log(n1)
-    n1++
+function canal(){
+    let n1 = 13
+    let n2 = 5
+    let res=n1*n2
+    if(res%2==0)
+        return "par"
+    else
+        return "Impar"
 }
+
+let res=canal()
+
+console.log(res)
 
